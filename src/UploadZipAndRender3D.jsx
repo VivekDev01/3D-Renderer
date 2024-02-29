6ba8ec6f5b5265cd5ecefb2325c3e17719776425
Vivek-BioCliq <vivek@biocliq.com>
2024-02-29 17:23:26 +0530

    render all the json files extracted from the zip file

--- src/UploadZipAndRender3D.jsx
+++ src/UploadZipAndRender3D.jsx
@@ -1,96 +1,11 @@
 import React, { useRef, useState } from 'react';
-import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
-import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkMTLReader from '@kitware/vtk.js/IO/Misc/MTLReader';
-import vtkOBJReader from '@kitware/vtk.js/IO/Misc/OBJReader';
 import JSZip from 'jszip';
 import { useEffect } from 'react';
+import JsonFileRenderer from './components/JsonFileRenderer';
 
 const UploadZipAndRender3D = () => {
-  const containerRef = useRef(null);
   const zipInputRef = useRef(null);
   const [jsonFiles, setJsonFiles]=useState([])
-
-  const initialize3DRenderer = async (jsonFile) => {
-    console.log('Initializing 3D renderer...');
-    const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
-      container: containerRef.current,
-      background: [0.5, 0.5, 0.5],
-    });
-    const renderer = fullScreenRenderer.getRenderer();
-    const renderWindow = fullScreenRenderer.getRenderWindow();
-
-    const resetCamera = renderer.resetCamera;
-    const render = renderWindow.render;
-
-    const reader = vtkOBJReader.newInstance({ splitMode: 'usemtl' });
-    const materialsReader = vtkMTLReader.newInstance();
-    const scene = [];
-    console.log('Materials and reader set.');
-
-    function onClick(event) {
-      const el = event.target;
-      const index = Number(el.dataset.index);
-      const actor = scene[index].actor;
-      const visibility = actor.getVisibility();
-
-      actor.setVisibility(!visibility);
-      if (visibility) {
-        el.classList.remove('visible');
-      } else {
-        el.classList.add('visible');
-      }
-      render();
-    }
-
-    try {
-        const jsonText = jsonFile.jsonData
-        const objText = jsonText.obj;
-        reader.parseAsText(objText);
-        const mtlText = jsonText.mtl;
-        materialsReader.parseAsText(mtlText);
-        
-
-        const size = reader.getNumberOfOutputPorts();
-        console.log('size', size);
-        for (let i = 0; i < size; i++) {
-            const polydata = reader.getOutputData(i);
-            const name = polydata.get('name').name;
-            const mapper = vtkMapper.newInstance();
-            const actor = vtkActor.newInstance();
-
-            actor.setMapper(mapper);
-            mapper.setInputData(polydata);
-
-            materialsReader.applyMaterialToActor(name, actor);
-            renderer.addActor(actor);
-
-            scene.push({ name, polydata, mapper, actor });
-        }
-        resetCamera();
-        render();
-
-        const htmlBuffer = [
-            '<style>.visible { font-weight: bold; } .click { cursor: pointer; min-width: 150px;}</style>',
-        ];
-        scene.forEach((item, idx) => {
-            htmlBuffer.push(
-            `<div class="click visible" data-index="${idx}">${item.name}</div>`
-            );
-        });
-
-        fullScreenRenderer.addController(htmlBuffer.join('\n'));
-        const nodes = document.querySelectorAll('.click');
-        for (let i = 0; i < nodes.length; i++) {
-            const el = nodes[i];
-            el.onclick = onClick;
-        }
-        console.log('Renderer initialization complete.');
-    } catch (error) {
-      console.error('Error initializing 3D renderer:', error);
-    }
-  };
 
   const handleSubmit=async(e)=>{
     e.preventDefault();
@@ -115,8 +30,6 @@ const UploadZipAndRender3D = () => {
           });
           const extractedJsonData = await Promise.all(promises);
           setJsonFiles(extractedJsonData);
-          console.log('Extracted JSON Files.');
-          initialize3DRenderer(extractedJsonData[0])
         } catch (error) {
           console.error('Error extracting or processing files:', error);
         }
@@ -129,9 +42,6 @@ const UploadZipAndRender3D = () => {
   };
 
 
-  useEffect(()=>{
-    console.log(jsonFiles)
-  }, [jsonFiles])
 
 
   return (
@@ -145,7 +55,15 @@ const UploadZipAndRender3D = () => {
         <button type="submit">Submit</button>
       </form>
 
-      <div ref={containerRef} />
+      
+      <div>
+        {jsonFiles.map((jsonFile, index)=>(
+          <div key={index} >
+            <JsonFileRenderer jsonFile={jsonFile} />
+          </div>
+        ))}
+      </div>
+      
     </>
   );
 };
---
+++ src/components/JsonFileRenderer.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
+import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
+import vtkMTLReader from '@kitware/vtk.js/IO/Misc/MTLReader';
+import vtkOBJReader from '@kitware/vtk.js/IO/Misc/OBJReader';
+import { useEffect, useRef } from 'react';
+
+const JsonFileRenderer = (props) => {
+  const containerRef = useRef(null);
+
+
+    const initialize3DRenderer = async (jsonFile) => {
+        console.log('Initializing 3D renderer...');
+        const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+          container: containerRef.current,
+          background: [0.5, 0.5, 0.5],
+        });
+        const renderer = fullScreenRenderer.getRenderer();
+        const renderWindow = fullScreenRenderer.getRenderWindow();
+    
+        const resetCamera = renderer.resetCamera;
+        const render = renderWindow.render;
+    
+        const reader = vtkOBJReader.newInstance({ splitMode: 'usemtl' });
+        const materialsReader = vtkMTLReader.newInstance();
+        const scene = [];
+        console.log('Materials and reader set.');
+    
+        function onClick(event) {
+          const el = event.target;
+          const index = Number(el.dataset.index);
+          const actor = scene[index].actor;
+          const visibility = actor.getVisibility();
+    
+          actor.setVisibility(!visibility);
+          if (visibility) {
+            el.classList.remove('visible');
+          } else {
+            el.classList.add('visible');
+          }
+          render();
+        }
+    
+        try {
+            const jsonText = jsonFile.jsonData
+            const objText = jsonText.obj;
+            reader.parseAsText(objText);
+            const mtlText = jsonText.mtl;
+            materialsReader.parseAsText(mtlText);
+            
+    
+            const size = reader.getNumberOfOutputPorts();
+            console.log('size', size);
+            for (let i = 0; i < size; i++) {
+                const polydata = reader.getOutputData(i);
+                const name = polydata.get('name').name;
+                const mapper = vtkMapper.newInstance();
+                const actor = vtkActor.newInstance();
+    
+                actor.setMapper(mapper);
+                mapper.setInputData(polydata);
+    
+                materialsReader.applyMaterialToActor(name, actor);
+                renderer.addActor(actor);
+    
+                scene.push({ name, polydata, mapper, actor });
+            }
+            resetCamera();
+            render();
+    
+            const htmlBuffer = [
+                '<style>.visible { font-weight: bold; } .click { cursor: pointer; min-width: 150px;}</style>',
+            ];
+            scene.forEach((item, idx) => {
+                htmlBuffer.push(
+                `<div class="click visible" data-index="${idx}">${item.name}</div>`
+                );
+            });
+    
+            fullScreenRenderer.addController(htmlBuffer.join('\n'));
+            const nodes = document.querySelectorAll('.click');
+            for (let i = 0; i < nodes.length; i++) {
+                const el = nodes[i];
+                el.onclick = onClick;
+            }
+            console.log('Renderer initialization complete.');
+        } catch (error) {
+          console.error('Error initializing 3D renderer:', error);
+        }
+      };
+
+      useEffect(() => {
+        const jsonFile = props.jsonFile;
+      
+        if (jsonFile && jsonFile.jsonData) {
+          initialize3DRenderer(jsonFile);
+        } else {
+          console.error('Invalid jsonFile or jsonData is undefined.');
+        }
+      }, []);
+      
+
+
+  return (
+    <div ref={containerRef} />
+
+  )
+}
+
+export default JsonFileRenderer
